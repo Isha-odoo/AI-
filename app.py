911794cb8c4383d59647fadcf2761cf8eadf14a7
Isha-odoo <isha3031998@gmail.com>
2026-05-01 17:40:47 +0530

    Configure app to run on specified port

--- app.py
+++ app.py
@@ -138,4 +138,6 @@ def extract():
 # RUN APP
 # =========================
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
